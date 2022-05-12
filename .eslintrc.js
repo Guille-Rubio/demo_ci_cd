79f7aa3b987af75cf803d3ddf799edde8be071b7
Guille-Rubio <guillermorubiog@gmail.com>
2022-05-12 11:23:28 +0200

    ADD jest to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  env: {
+  eenv: {
     browser: true,
-    es2020: true
+    es2020: true,
+    node: true,
+    jest: true //admitir las palabras clave de jest 
   },
+  
   extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: 12,
